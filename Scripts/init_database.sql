c70a0b87b860f971e61b32f77a5aa44ef0c1c844
yamini515774 <yamini515774@gmail.com>
2026-09-09 15:35:21 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,1 +1,22 @@
+use master;
+-- create database Datawareouse 
+IF DB_ID('DataWarehouse') IS NOT NULL
+BEGIN
+    ALTER DATABASE DataWarehouse
+    SET SINGLE_USER
+    WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE DataWarehouse;
+END;
+go
+create database Datawarehouse;
+use Datawarehouse;
+-- creating schemas bronze,silver,gold
+go
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
 
